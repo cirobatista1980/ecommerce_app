9ba0d5a8cadb66a81c90a7039975a11823503350
Desenv - Ciro Batista <bc0025@daycoval.daycovalnet.com.br>
2019-07-29 15:52:54 -0300

    Requeste Http para validar usuario

--- app/src/main/java/br/com/meadme/ecommerce/MainActivity.java
+++ app/src/main/java/br/com/meadme/ecommerce/MainActivity.java
@@ -11,45 +11,38 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.QuickContactBadge;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnEntrar;
+    private Button btnCriar;
+    private EditText edUsuario;
+    private EditText edSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void carregarControles() {
+        btnEntrar = findViewById(R.id.btn_entrar);
+        btnCriar = findViewById(R.id.btn_criar);
+        edUsuario = findViewById(R.id.edUsuario);
+        edSenha = findViewById(R.id.edSenha);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void carregaEventos() {
+        btnEntrar.setOnClickListener(view -> autenticarUsuario());
+        btnCriar.setOnClickListener(view -> criarUsuario());
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    private void criarUsuario() {
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void autenticarUsuario() {
     }
 }
---
+++ app/src/main/java/br/com/meadme/ecommerce/Models/Usuario.java
@@ -0,0 +1,49 @@
+package br.com.meadme.ecommerce.Models;
+
+public class Usuario {
+    private int Codigo;
+    private String Nome;
+    private String Sobrenome;
+    private String Login;
+    private String Senha;
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        Codigo = codigo;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getSobrenome() {
+        return Sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        Sobrenome = sobrenome;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String login) {
+        Login = login;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String senha) {
+        Senha = senha;
+    }
+}
